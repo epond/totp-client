80e1ed6c203a7edafa688607794f35ebc82a72cb
epond <epond@users.noreply.github.com>
2018-01-27 16:03:46 +0000

    read secrets from file

--- account/account.go
+++ account/account.go
@@ -1,13 +1,34 @@
 package account
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"path"
+	"strings"
+)
+
+const line_separator = " "
+
 type TotpAccount struct {
 	Name   string
 	Secret string
 }
 
 func TotpAccounts() []TotpAccount {
-	return []TotpAccount{
-		{"Good account", "abcde"},
-		{"Bad account", "💩"},
+	accounts := make([]TotpAccount, 0)
+	secretsfile, err := os.Open(path.Join(os.Getenv("HOME"), "totpsecrets"))
+	if err != nil {
+		log.Fatalf("Could not open root folder. Cause: %v", err)
 	}
+	scanner := bufio.NewScanner(secretsfile)
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), line_separator)
+		if len(line) != 2 {
+			log.Fatalf("Expected two values per line separated by '%v'. Instead found %v values.", line_separator, len(line))
+		}
+		accounts = append(accounts, TotpAccount{line[0], line[1]})
+	}
+	secretsfile.Close()
+	return accounts
 }
